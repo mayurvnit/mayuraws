1e7102596f933f5f9e459fa209e640cc4e400531
mayurvnit <mayurvnit@gmail.com>
2014-07-16 21:11:43 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mayuraws::Application.config.secret_key_base = '1e897beb96027e9fea94adcf2e446cd8a56bfcd2c57090530d839715f0bb2716816818cbdcba8c6b9cf436bd60a167ba2197d5fbad22c5a6ad820ff325fc59d2'
